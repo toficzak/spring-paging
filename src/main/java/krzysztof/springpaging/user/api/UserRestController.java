f4ccf1449fbf271f56deb4119d256b32d72fc001
Krzysztof Kucharczyk <krzysztof.kucharczyk@hotmail.com>
2020-02-29 22:00:47 +0100

    Basic paging.

--- src/main/java/krzysztof/springpaging/user/api/UserRestController.java
+++ src/main/java/krzysztof/springpaging/user/api/UserRestController.java
@@ -2,10 +2,15 @@ package krzysztof.springpaging.user.api;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import krzysztof.springpaging.user.domain.User;
@@ -24,9 +29,21 @@ public class UserRestController {
 	}
 
 	@GetMapping
-	public List<UserDto> listing() {
-		Set<User> users = new HashSet<User>();
-		userRepository.findAll().forEach(users::add);
-		return UserMapper.toDto(users);
+	public List<UserDto> listing(
+			@RequestParam("page") Optional<Integer> optPage,
+			@RequestParam("size") Optional<Integer> optSize) {
+
+		Iterable<User> users;
+		if (optPage.isPresent() && optSize.isPresent()) {
+			Pageable pageable = PageRequest.of(optPage.get(), optSize.get(),
+					Sort.by("id"));
+			users = userRepository.findAll(pageable);
+		} else {
+			users = userRepository.findAll();
+		}
+
+		Set<User> uniqueUsers = new HashSet<User>();
+		users.forEach(uniqueUsers::add);
+		return UserMapper.toDto(uniqueUsers);
 	}
 }
--- src/main/java/krzysztof/springpaging/user/domain/UserRepository.java
+++ src/main/java/krzysztof/springpaging/user/domain/UserRepository.java
@@ -1,9 +1,9 @@
 package krzysztof.springpaging.user.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
 }
